c8ae8ca94b913a57d88aae01236364d1ae7f9eba
tsxepo <tsxepo@gmail.com>
2023-06-04 22:39:45 +0200

    Refactor

--- TriviaApp/Program.cs
+++ TriviaApp/Program.cs
@@ -18,13 +18,11 @@ builder.Services.AddSingleton(mongoCollection);
 builder.Services.AddScoped<ITriviaAppRepository, TriviaAppRepository>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
